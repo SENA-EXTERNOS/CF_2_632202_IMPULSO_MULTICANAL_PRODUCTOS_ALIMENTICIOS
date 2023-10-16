0a6fae34bc31878203817e8442158cee919e448f
jorozcop <orojacobo@gmail.com>
2023-10-16 16:38:38 -0500

    Ajustes 16-10-2023

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -10,9 +10,9 @@
     .row.justify-content-center.mt-5
       .col-11.col-lg-7
         .cajon.color-acento-botones.p-4
-          p El Sistema de Gestión de la Seguridad y Salud en el Trabajo (SG SST) históricamente ha estado evolucionando y reformándose por medio de normativas y decretos que van reglamentando obligatoriedad de su cumplimiento. Este inició con los programas de salud ocupacional, los cuales consistían en la planeación y ejecución de actividades de medicina, seguridad e higiene industrial con el objetivo de mantener y mejorar la salud de los trabajadores.
+          p El Sistema de Gestión de la Seguridad y Salud en el Trabajo (SG-SST) históricamente ha estado evolucionando y reformándose por medio de normativas y decretos que van reglamentando obligatoriedad de su cumplimiento. Este inició con los programas de salud ocupacional, los cuales consistían en la planeación y ejecución de actividades de medicina, seguridad e higiene industrial con el objetivo de mantener y mejorar la salud de los trabajadores.
             br
-            |Para finalizar este componente formativo, se hace una recopilación del cumplimiento de la norma que regula la SG SST, descripción de los elementos de protección que debe utilizar el trabajador y las medidas de prevención asociadas. 
+            |Para finalizar este componente formativo, se hace una recopilación del cumplimiento de la norma que regula la SG-SST, descripción de los elementos de protección que debe utilizar el trabajador y las medidas de prevención asociadas. 
 
         
 
@@ -34,7 +34,7 @@
 
       .col-11.col-lg-7.mt-3
         p La Seguridad y Salud en el Trabajo (SST) es una actividad que involucra todos los niveles de la sociedad y disciplinas laborales, se enfoca en proteger y promover beneficios en la salud física, mental y emocional de los trabajadores mediante actividades de prevención de accidentes y enfermedades laborales. 
-        p En el año 2012 fue necesaria la actualización de la normativa colombiana, lo que dio lugar a la Ley 1562 de 2012, cuyo principal aporte consistió en reemplazar el Programa de Salud Ocupacional, por el Sistema de Gestión de la Seguridad y la Salud en el Trabajo SG SST. La reglamentación de esta ley llegó dos años después, con el Decreto 1443 de 2014, el cual constituye un manual para implementar el SG SST en todas las organizaciones. (Safetya, 2015) 
+        p En el año 2012 fue necesaria la actualización de la normativa colombiana, lo que dio lugar a la Ley 1562 de 2012, cuyo principal aporte consistió en reemplazar el Programa de Salud Ocupacional, por el Sistema de Gestión de la Seguridad y la Salud en el Trabajo SG-SST. La reglamentación de esta ley llegó dos años después, con el Decreto 1443 de 2014, el cual constituye un manual para implementar el SG SST en todas las organizaciones. (Safetya, 2015) 
 
     separador
 
@@ -110,25 +110,25 @@
       .col-11.col-lg-8.mb-3
         LineaTiempoD
           .row(numero="1" titulo="Señalizar: ")
-            p al momento de realizar un proceso de desinfección o aseo importante, es imperativo marcar el área con el fin de evitar algún tipo de caída a nivel, contacto con algún químico u otro tipo de accidente que se pueda presentar.
+            p Al momento de realizar un proceso de desinfección o aseo importante, es imperativo marcar el área con el fin de evitar algún tipo de caída a nivel, contacto con algún químico u otro tipo de accidente que se pueda presentar.
           
           .row(numero="2" titulo="Pausas activas:")
-            p son espacios que permiten a los trabajadores estirar sus músculos, cambiar de la actividad en la que se encuentran, descansar un poco con ánimo de evitar estrés repetitivo en sus extremidades u otro tipo de lesión.
+            p Son espacios que permiten a los trabajadores estirar sus músculos, cambiar de la actividad en la que se encuentran, descansar un poco con ánimo de evitar estrés repetitivo en sus extremidades u otro tipo de lesión.
 
           .row(numero="3" titulo="Ritmos de trabajo:")
-            p evitar los ritmos de trabajo acelerados, ya que puede generar algún tipo de lesión muscular.
+            p Evitar los ritmos de trabajo acelerados, ya que puede generar algún tipo de lesión muscular.
 
           .row(numero="4" titulo="Evitar riesgos físicos:")
-            p estar muy atento al entorno y a los riesgos a los que se está expuesto, como por ejemplo puertas corredizas, pasos a desnivel, escaleras, entre otros.
+            p Estar muy atento al entorno y a los riesgos a los que se está expuesto, como por ejemplo puertas corredizas, pasos a desnivel, escaleras, entre otros.
 
           .row(numero="5" titulo="Utilizar los elementos de protección:")
-            p cuando el caso lo requiera, como por ejemplo exposición a temperaturas extremas o exposición al ruido.
+            p Cuando el caso lo requiera, como por ejemplo exposición a temperaturas extremas o exposición al ruido.
 
           .row(numero="6" titulo="Revisar cuidadosamente las etiquetas de los productos de limpieza:")
-            p con el fin de evitar algún tipo de mezcla que pueda ocasionar un incendio u otro tipo de alteración.
+            p Con el fin de evitar algún tipo de mezcla que pueda ocasionar un incendio u otro tipo de alteración.
 
           .row(numero="7" titulo="Posiciones correctas:")
-            p al momento de levantar peso para surtir estantería, hacerlo con los pies debidamente apoyados y la espalda recta en todo momento.
+            p Al momento de levantar peso para surtir estantería, hacerlo con los pies debidamente apoyados y la espalda recta en todo momento.
       
       .col-6.col-lg-4
         figure
